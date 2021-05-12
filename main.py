67392d7cca7517a6d1b71cfd3b332345782151ef
Nils Moynac <e12016308@student.tuwien.ac.at>
2021-05-12 22:14:49 +0200

    doesn't work yet

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import traffic_jam_functions as tjf
 import matplotlib.pyplot as plt
 
 # initialize the CA
-cellular_automaton = np.ones((1, n_rows, n_cols)) * (-1)
-cellular_automaton[0, :, 0] = np.random.randint(0, max_model_speed, n_rows)
 
+cellular_automaton = np.ones((n_rows, n_cols,2)) * (-1)
+cellular_automaton[:, 0, 0] = np.random.randint(0, max_model_speed, n_rows)
+cellular_automaton[:, 0, 1] = [i for i in range(n_rows)]
+print(cellular_automaton.shape)
 # evolve the cellular automaton for n_time_steps time steps
 cellular_automaton = tjf.evolve2d(cellular_automaton, timesteps=n_time_steps,
                                   apply_rule=lambda n, c, t: tjf.traffic_jam_rule(n, c, t), r=radius)
-
-tjf.plot2d_animate(cellular_automaton)
+values = cellular_automaton[:,:,:,0]
+print(values.shape)
+tjf.plot2d_animate(values)
 # for i in range(1, 5):
 #    tjf.plot2d(cellular_automaton, i)
 
--- traffic_jam_functions.py
+++ traffic_jam_functions.py
@@ -3,13 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from settings import *
-
+maxIndex = n_rows
 
 def evolve2d(cellular_automaton, timesteps, apply_rule, r=1):
-    _, rows, cols = cellular_automaton.shape
-    array = np.zeros((timesteps, rows, cols), dtype=cellular_automaton.dtype)
+    rows, cols, _ = cellular_automaton.shape
+    array = np.zeros((timesteps, rows, cols, 2), dtype=cellular_automaton.dtype)
     array[0] = cellular_automaton
-
     def get_neighbourhood(cell_layer, row, col):
         # Neighborhood of traffic jam:
         col_indices = range(col - r, col + r + 1)
@@ -19,7 +18,7 @@ def evolve2d(cellular_automaton, timesteps, apply_rule, r=1):
         return cell_layer[np.ix_(row_indices, col_indices)]
 
     for t in range(1, timesteps):
-        cell_layer = array[t - 1]
+        cell_layer = array[t - 1] # cell_layer represents the grid at time t-1
         for row, cell_row in enumerate(cell_layer):
             for col, cell in enumerate(cell_row):
                 n = get_neighbourhood(cell_layer, row, col)
@@ -29,8 +28,8 @@ def evolve2d(cellular_automaton, timesteps, apply_rule, r=1):
 
 def value_is_of_interest(index, cell, next_cell):
     # The value of interest will reach the current cell in the next time step
-    car_will_reach_cell = index == cell
-    car_has_to_break = next_cell != -1 and cell > index
+    car_will_reach_cell = index == cell[0]
+    car_has_to_break = next_cell[0] != -1 and cell[0] > index
     return car_will_reach_cell or car_has_to_break
 
 
@@ -47,11 +46,11 @@ def traffic_jam_rule(neighborhood, c, t):
     one_after_current_cell = important_cells.pop()  # value after the current cell
     important_cells.reverse()
     result = next(((ind, c) for ind, c in enumerate(list(important_cells)) if
-                   value_is_of_interest(ind, c, one_after_current_cell)), (-1, -1))
-    (index_of_interest, value_of_interest) = result
+                   value_is_of_interest(ind, c, one_after_current_cell)), (-1, (-1,6)))
+    (index_of_interest, cell_of_interest) = result
 
     curr_cell = important_cells[0]  # value in the current cell
-    if col == 0 and curr_cell == -1 and random.random() < prop_new_car:
+    if col == 0 and curr_cell[0] == -1 and random.random() < prop_new_car:
         # Cars will appear randomly at the beginning of each column, if there is space
         return random.randint(1, max_model_speed)
     elif col == n_cols - 1:
@@ -67,16 +66,16 @@ def traffic_jam_rule(neighborhood, c, t):
         cells_to_consider = curr_lane[index_in_correct_order + 1:]
         # To define new value, consider the cells ahead
         try:
-            gap_size = list(cells_to_consider).index(next(v for v in cells_to_consider if v != -1))
+            gap_size = list(cells_to_consider[:,0]).index(next(v[0] for v in cells_to_consider if v[0] != -1))
             # Gap size is the space until the next cell with a car in it
         except StopIteration:
             gap_size = max_model_speed + 1
             # If no car within radius is found, the gap is wider than max_model_speed and thus irrelevant
-        if gap_size <= value_of_interest:
+        if gap_size <= cell_of_interest[0]:
             return_value = gap_size
             # A car within radius will break out a car behind it
-        elif value_of_interest < max_model_speed:
-            return_value = value_of_interest + 1
+        elif cell_of_interest[0] < max_model_speed:
+            return_value = cell_of_interest + 1
             # Speed accelerates if no car is within radius and max_speed is not yet reached
         else:
             return_value = max_model_speed
